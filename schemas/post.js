a10905779ec083bde6e7ae31835d609a1b41b251
Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>
2023-03-31 22:36:26 +0300

    Project cloning

--- schemas/post.js
+++ schemas/post.js
@@ -1,30 +1,54 @@
+const textEditorStyles = [
+	{ title: 'Paragraph', value: 'normal'},
+	{ title: 'Heading 1', value: 'h1'},
+	{ title: 'Heading 2', value: 'h2'},
+	{ title: 'Heading 3', value: 'h3'},
+	{ title: 'Bullet', value: 'bullet'},
+	{ title: 'Numbered', value: 'number'},
+	{ title: 'Quote', value: 'blockquote'},
+]
+
 export default {
   name: 'post',
   type: 'document',
 	title: 'Post',
+	groups: [
+    {
+			name:'content',
+			title: 'Content',
+    },
+		{
+			name: 'meta',
+			title: 'Meta'
+		}
+	],
   fields: [
     {
       name: 'meta_title',
       type: 'string',
       title: 'Meta title',
 			validation: Rule => Rule.required(),
+			group: 'meta',
     },
 		{
 			name: 'title',
       type: 'string',
       title: 'Title',
+			group: 'content',
 			validation: Rule => Rule.required(),
 		},
 		{
 			name: 'publishDate',
 			type: 'date',
 			title: 'Pablished Date',
+			group: 'content',
 			validation: Rule => Rule.required(),
 		},
 		{
 			name: 'image',
       type: 'image',
       title: 'Image',
+			group: 'content',
 			validation: Rule => Rule.required(),
 			fields: [
 				{
@@ -43,6 +67,7 @@ export default {
 			name: 'slug',
 			type: 'slug',
 			title: 'Slug',
+			group: 'content',
 			validation: Rule => Rule.required(),
 			options: {
 				source: 'title',
@@ -57,11 +82,13 @@ export default {
 			title: 'Description',
 			name: 'description',
 			type: 'text',
+			group: 'content',
 			validation: Rule => Rule.required(),
 		},
 		{
 			name: 'body',
       title: 'Body content',
+			group: 'content',
 			validation: Rule => Rule.required(),
 			type: 'array',
 			of: [
